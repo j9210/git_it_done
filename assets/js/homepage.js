241604f11d1f27f30a61d26b40a715313634dc99
j9210 <71297542+j9210@users.noreply.github.com>
2020-10-31 23:32:13 -0500

    Get user functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,7 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+// connect serverside API that will get user repos
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -9,5 +13,21 @@ var getUserRepos = function(user) {
         });
     });
 };
-console.log("outside");
-getUserRepos();
+
+// function to be executed upon a form submission browser event
+var formSubmitHandler = function(event) {
+    // prevents the browser from sending the form's input data to a URL
+    event.preventDefault();
+    
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos (username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
