16d686cd85bbf9871f5343b79d529da13afb7ac4
j9210 <71297542+j9210@users.noreply.github.com>
2020-11-01 00:03:25 -0500

    error notifications

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,11 +10,22 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
+    fetch(apiUrl)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        // request was unsuccesful
+        .catch(function(error){
+            // Notice this '.catch()' getting chained onto the end of the '.then()'
+            alert("Unable to connect to GitHub");
         });
-    });
 };
 
 // function to be executed upon a form submission browser event
@@ -35,6 +46,11 @@ var formSubmitHandler = function(event) {
 
 // function to display repos
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     // clear old content
@@ -71,7 +87,7 @@ var displayRepos = function(repos, searchTerm) {
 
         // append to container 
         repoEl.appendChild(statusEl);
-        
+
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
     }
